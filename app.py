3f20f774eaea3251f6166c9e467ce4325bcdf75d
GroovieGermanikus <groovie@mango.markets>
2023-10-13 21:13:39 +0200

    copy styles from explorer

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import dbtestcall
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/txerrors')
+def table():
+    rows = dbtestcall.some_data()
+    return render_template('table.html', rows=rows)
+
 @app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
