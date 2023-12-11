34d93c811c0a76343ab64d52d76a1c5d3d58dac9
GroovieGermanikus <groovie@mango.markets>
2023-12-11 08:07:07 +0100

    read strcuture

--- transaction_details_database.py
+++ transaction_details_database.py
@@ -9,6 +9,7 @@ def find_transaction_details_by_sig(tx_sig: str):
     maprows = postgres_connection.query(
         """
         SELECT
+            txi.transaction_id,
             signature,
             '{}'::text[] all_errors,--  FIXME
             is_successful,
@@ -32,6 +33,50 @@ def find_transaction_details_by_sig(tx_sig: str):
     if maprows:
         row = maprows[0]
 
+        # {'transaction_id': 1039639, 'slot': 234765028, 'error': 34, 'count': 1, 'utc_timestamp': datetime.datetime(2023, 12, 8, 18, 29, 23, 861619)}
+        tx_slots = postgres_connection.query(
+            """
+            SELECT
+             txslot.slot,
+             txslot.error,
+             err.error
+            FROM banking_stage_results_2.transaction_slot txslot
+            INNER JOIN banking_stage_results_2.errors err ON err.error_code=txslot.error
+            WHERE transaction_id=%s
+            """, args=[row["transaction_id"]])
+        # ordered by slots ascending
+        relevant_slots = set([txslot["slot"] for txslot in tx_slots])
+
+        accountinfos_per_slot =(
+            invert_by_slot(
+                postgres_connection.query(
+                """
+                SELECT
+                 amb.*,
+                 acc.account_key
+                FROM banking_stage_results_2.accounts_map_blocks amb
+                INNER JOIN banking_stage_results_2.accounts acc ON acc.acc_id=amb.acc_id
+                WHERE slot IN (SELECT unnest(CAST(%s as bigint[])))
+                """, args=[relevant_slots]))
+        )
+
+        print("- transaction details for sig: " + tx_sig)
+        print("- relevant slots: " + str(relevant_slots))
+        for relevant_slot in relevant_slots:
+            accountinfos = accountinfos_per_slot.get(relevant_slot, [])
+            print("  - slot: ", relevant_slot)
+            print("  - errors in slot:")
+            for tx_slots_row in tx_slots:
+                if tx_slots_row['slot'] == relevant_slot:
+                    print("    - " + tx_slots_row['error'])
+            print("  - write-locked accounts: ")
+            for account in accountinfos:
+                if account['is_write_locked']:
+                    print("    - ", account['account_key'])
+            print("  - read-locked accounts:")
+            for account in accountinfos:
+                if not account['is_write_locked']:
+                    print("    - ", account['account_key'])
 
     return maprows
 
